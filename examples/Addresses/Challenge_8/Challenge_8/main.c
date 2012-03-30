e8cc17eae724b8e37459658ef21128c2d26cb7a3
Thais Camilo <narwen@gmail.com>
2012-03-29 22:53:39 -0400

    Challenge 8

---
+++ examples/Addresses/Challenge_8/Challenge_8/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  Challenge_8
+//
+//  Created by Thais Camilo on 3/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main (int argc, const char * argv[])
+{
+    printf("Float consumes %zu bytes\n", sizeof(float));
+    return 0;
+}
+
