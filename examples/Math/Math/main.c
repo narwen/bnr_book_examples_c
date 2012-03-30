4b444bfd8357a29c42af51bdbe5be8965cc559e5
Thais Camilo <narwen@gmail.com>
2012-03-29 21:53:51 -0400

    Chapter 6 - Use Math

---
+++ examples/Math/Math/main.c
@@ -0,0 +1,19 @@
+//
+//  main.c
+//  Math
+//
+//  Created by Thais Camilo on 3/29/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <stdio.h>
+#include <math.h>
+
+int main (int argc, const char * argv[])
+{
+    double result;
+    result = sin(1);
+    printf("Sin %.3f\n", result);
+    return 0;
+}
+
