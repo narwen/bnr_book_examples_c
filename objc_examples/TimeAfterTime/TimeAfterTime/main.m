b95c03449dfdf51d0fa8982886dcfb359ed7a07c
Thais Camilo <narwen@gmail.com>
2012-04-07 21:52:57 -0400

    Challenge 12

--- objc_examples/TimeAfterTime/TimeAfterTime/main.m
+++ objc_examples/TimeAfterTime/TimeAfterTime/main.m
@@ -23,6 +23,20 @@ int main (int argc, const char * argv[])
         NSDate *later = [now dateByAddingTimeInterval:1000000];
         NSLog(@"In 100,000 seconds will be %@", later);
         
+        NSDateComponents *comps = [[NSDateComponents alloc] init];
+        [comps setYear:1982];
+        [comps setMonth:2];
+        [comps setDay:17];
+        [comps setHour:16];
+        [comps setMinute:4];
+        [comps setSecond:4];
+        
+        NSCalendar *g = [[NSCalendar alloc] initWithCalendarIdentifier:NSGregorianCalendar];
+        NSDate *dateOfBirth = [g dateFromComponents:comps];
+        
+        double d = [now timeIntervalSinceDate:dateOfBirth];
+        NSLog(@"Seconds since I was born %f", d);
+        
     }
     return 0;
 }
